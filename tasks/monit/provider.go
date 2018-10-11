45f5c93f95a4bc4f086dd0e5205c58eb0d48c7d9
Oleksandr Slynko <oslynko@pivotal.io>
2018-10-11 18:19:46 +0100

    Changes that were only in release repo

--- tasks/monit/provider.go
+++ tasks/monit/provider.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
-	boshhttp "github.com/cloudfoundry/bosh-utils/httpclient"
+	boshhttp "github.com/cloudfoundry/bosh-utils/http"
 	boshlog "github.com/cloudfoundry/bosh-utils/logger"
 	boshsys "github.com/cloudfoundry/bosh-utils/system"
 )
